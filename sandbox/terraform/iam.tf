08d7db2088ce1660384792c962e0595605d0f6be
MBO-at-157470516119 <muhammad.bouabid92@gmail.com>
2024-10-07 16:59:35 +0200

    comment IAM Permission to SecretManager

--- sandbox/terraform/iam.tf
+++ sandbox/terraform/iam.tf
@@ -1,41 +1,41 @@
-resource "aws_iam_policy" "secrets_manager_policy" {
-  name        = "SecretsManagerPolicy"
-  path        = "/"
-  description = "My test policy"
+# resource "aws_iam_policy" "secrets_manager_policy" {
+#   name        = "SecretsManagerPolicy"
+#   path        = "/"
+#   description = "My test policy"
 
 
-  policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "secretsmanager:ListSecrets",
-        "secretsmanager:GetSecretValue"
-      ],
-      "Resource": "*"
-    }
-  ]
-})
-}
+#   policy = jsonencode({
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": [
+#         "secretsmanager:ListSecrets",
+#         "secretsmanager:GetSecretValue"
+#       ],
+#       "Resource": "*"
+#     }
+#   ]
+# })
+# }
 
-resource "aws_iam_role" "secrets_manager_role" {
-  name               = "SecretsManagerAccessRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        AWS = "${data.aws_caller_identity.current.arn}"
-      }
-    }]
-  })
-}
+# resource "aws_iam_role" "secrets_manager_role" {
+#   name               = "SecretsManagerAccessRole"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action    = "sts:AssumeRole"
+#       Effect    = "Allow"
+#       Principal = {
+#         AWS = "${data.aws_caller_identity.current.arn}"
+#       }
+#     }]
+#   })
+# }
 
 
-resource "aws_iam_policy_attachment" "secrets_manager_attachment" {
-  name       = "SecretsManagerPolicyAttachment"
-  policy_arn = aws_iam_policy.secrets_manager_policy.arn
-  roles      = [aws_iam_role.secrets_manager_role.name]
-}
+# resource "aws_iam_policy_attachment" "secrets_manager_attachment" {
+#   name       = "SecretsManagerPolicyAttachment"
+#   policy_arn = aws_iam_policy.secrets_manager_policy.arn
+#   roles      = [aws_iam_role.secrets_manager_role.name]
+# }
