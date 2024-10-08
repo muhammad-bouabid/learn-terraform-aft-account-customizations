5cb673422f06caea972210356246a46637eb7da2
MBO-at-157470516119 <muhammad.bouabid92@gmail.com>
2024-10-08 22:06:18 +0200

    test permission to create EC2

--- sandbox/terraform/scp.tf
+++ sandbox/terraform/scp.tf
@@ -1,24 +1,49 @@
-#Create SCP for Management-Custom-OU to restrict launching EC2's type other than t2.micro
-data "aws_iam_policy_document" "scp-allow-ec2-microType-only" {
-  statement {
-    effect    = "Deny"
-    actions   = ["ec2:RunInstances"]
-    resources = ["arn:aws:ec2:*:*:instance/*"]
+# #Create SCP for Management-Custom-OU to restrict launching EC2's type other than t2.micro
+# data "aws_iam_policy_document" "scp-allow-ec2-microType-only" {
+#   statement {
+#     effect    = "Deny"
+#     actions   = ["ec2:RunInstances"]
+#     resources = ["arn:aws:ec2:*:*:instance/*"]
 
-    condition {
-      test = "ForAnyValue:StringNotEquals"
-      values = ["t2.micro"]
-      variable = "ec2:InstanceType"
-    }
+#     condition {
+#       test = "ForAnyValue:StringNotEquals"
+#       values = ["t2.micro"]
+#       variable = "ec2:InstanceType"
+#     }
+#   }
+# }
+
+# resource "aws_organizations_policy" "scp-allow-ec2-microType-only" {
+#   name    = "AllowOnlyEc2MicroTypeOnly"
+#   content = data.aws_iam_policy_document.scp-allow-ec2-microType-only.json
+# }
+
+# resource "aws_organizations_policy_attachment" "secondAccount" {
+#   policy_id = aws_organizations_policy.scp-allow-ec2-microType-only.id
+#   target_id = "${data.aws_caller_identity.current.account_id}"
+# }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_organizations_policy" "scp-allow-ec2-microType-only" {
-  name    = "AllowOnlyEc2MicroTypeOnly"
-  content = data.aws_iam_policy_document.scp-allow-ec2-microType-only.json
-}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
-resource "aws_organizations_policy_attachment" "secondAccount" {
-  policy_id = aws_organizations_policy.scp-allow-ec2-microType-only.id
-  target_id = "${data.aws_caller_identity.current.account_id}"
+  tags = {
+    Name = "HelloWorld"
+  }
 }
